d3cc9ad90a440ee86859427fcdd384ba1ea6d150
RShankar <ravi@rshankar.com>
2015-11-19 20:50:29 +0530

    Initial Commit

---
+++ FirebaseDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseDemo
+//
+//  Created by Ravi Shankar on 19/11/15.
+//  Copyright © 2015 Ravi Shankar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
